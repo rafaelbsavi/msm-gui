505c7855c6e9e7d09c7fb33c03a93ca0d60bb314
rafaelbsavi <73194818+rafaelbsavi@users.noreply.github.com>
2024-05-04 17:52:35 +0000

    Add edit form for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,6 +31,32 @@ class MoviesController < ApplicationController
   end
 
 
+  def update
+    #Get the ID out of params
+    m_id = params.fetch("the_id")
+
+    #Look up the existing record
+    matching_records = Movie.where({ :id => m_id})
+    the_movie = matching_records.at(0)
+
+    #Overwrite each column with the values from user inputs
+
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    #Save  
+    the_movie.save
+
+    #Redirect to the movie details page
+    redirect_to("/movies/#{the_movie.id}")
+
+  end 
+
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
